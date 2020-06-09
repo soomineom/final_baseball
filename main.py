8dbc8b2944b31dd106984624d43791a1776a5096
Soomin Julia <34619357+tnals5718@users.noreply.github.com>
2020-06-09 15:15:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 
 def createNewWindow():
   newWindow = Toplevel()
-  newCanvas = Canvas(newWindow, height=400, width=400)
+  newCanvas = Canvas(newWindow, height=400, width=600)
   newCanvas.pack()
   resultLabel = Label(newWindow, text = '<<{}월 {}일 오늘의 결과>>'.format(month, day))
   resultLabel.place(rely=0.03)
@@ -19,38 +19,40 @@ def createNewWindow():
   eLabel.place(rely=0.3)
 
   # 뉴스 크롤링
-  webpage = requests.get('https://sports.news.naver.com/kbaseball/news/index.nhn?isphoto=N&type=popular')
-  soup = BeautifulSoup(webpage.content, 'html.parser')
-  news = soup.select(".aside_rank_news>ul> li > a > span")
-  for k in range(10):
-      news[k] = str(news[k].text)
-  news_list=[]
-  for i in range(10):
-      news_list.append(news[i])
+  def newsCrawler():
+      webpage = requests.get('https://sports.news.naver.com/kbaseball/news/index.nhn?isphoto=N&type=popular')
+      soup = BeautifulSoup(webpage.content, 'html.parser')
+      news = soup.select(".aside_rank_news>ul> li > a > span")
+      for k in range(10):
+          news[k] = str(news[k].text)
+      news_list = []
+      for i in range(10):
+          news_list.append(news[i])
+      newsHead1 = Label(newsFrame, text="{}".format(news[0])).pack()
+      newsHead2 = Label(newsFrame, text="{}".format(news[1])).pack()
+      newsHead3 = Label(newsFrame, text="{}".format(news[2])).pack()
+      newsHead4 = Label(newsFrame, text="{}".format(news[3])).pack()
+      newsHead5 = Label(newsFrame, text="{}".format(news[4])).pack()
+      newsHead6 = Label(newsFrame, text="{}".format(news[5])).pack()
+      newsHead7 = Label(newsFrame, text="{}".format(news[6])).pack()
+      newsHead8 = Label(newsFrame, text="{}".format(news[7])).pack()
 
-
-  newsLabel = Label(newWindow, text = '<<{}월 {}일 오늘의 주요뉴스>>'.format(month, day))
+  #뉴스 출력/프레임
+  newsLabel = Label(newWindow, text = '<<{}월 {}일 오늘 실시간 인기뉴스>>'.format(month, day))
   newsLabel.place(rely=0.4)
   newsFrame = Frame(newWindow, bg='#eaeaea')
   newsFrame.place(relx=0.5, rely=0.45, relwidth=0.9, relheight=0.4, anchor='n')
-  newsHead1 = Label(newsFrame, text= "{}".format(news[0])).pack()
-  newsHead2 = Label(newsFrame, text= "{}".format(news[1])).pack()
-  newsHead3 = Label(newsFrame, text="{}".format(news[2])).pack()
-  newsHead4 = Label(newsFrame, text="{}".format(news[3])).pack()
-  newsHead5 = Label(newsFrame, text="{}".format(news[4])).pack()
-  newsHead6 = Label(newsFrame, text="{}".format(news[5])).pack()
-  newsHead7 = Label(newsFrame, text="{}".format(news[6])).pack()
-  newsHead8 = Label(newsFrame, text="{}".format(news[7])).pack()
+  newsCrawler()
 
 
   buttonFrame = Frame(newWindow)
   buttonFrame.place(relx=0.2, rely=0.9, relwidth=0.8, relheight=0.05)
   moveButton = Button(buttonFrame, text = '다른 날 보기')
   moveButton.place(relwidth=0.5, relheight=1)
-  exitButton = Button (buttonFrame, text = '닫기')
+  exitButton = Button (buttonFrame, text = '닫기') #닫기 후 창 종료 필요
   exitButton.place(relx=0.7, relwidth=0.1, relheight=1)
 
-
+#메인창
 root = Tk()
 root.title('Baseball Highlights')
 
