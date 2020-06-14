fc09925c3c90b8281827570000eeadc7665348ca
jykim12 <65805668+jykim12@users.noreply.github.com>
2020-06-14 15:10:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import urllib
 
 def createNewWindow():
     newWindow = Toplevel()
-    newCanvas = Canvas(newWindow, height=400, width=600)
+    newCanvas = Canvas(newWindow, height=500, width=700)
     newCanvas.pack()
     titleLabel = Label(newWindow, text='<<{}월 {}일 오늘의 일정/결과>>'.format(mm.get(), dd.get()))
     titleLabel.place(rely=0.03)
@@ -74,7 +74,7 @@ def createNewWindow():
             schLabel = Label(resultFrame, text="{}".format(",".join(map(str,final_res[i])))).pack()
 
     resultFrame = Frame(newWindow, bg='#eaeaea')
-    resultFrame.place(relx=0.5, rely=0.08, relwidth=0.9, relheight=0.3, anchor='n')
+    resultFrame.place(relx=0.5, rely=0.09, relwidth=0.9, relheight=0.235, anchor='n')
     get_result()
 
     # 뉴스 크롤링
@@ -90,27 +90,58 @@ def createNewWindow():
                 newsList.append(ns)
 
         for k in range(len(newsList)):
-            newsHead = Label(newsFrame, text="{}".format(newsList[k])).pack()
+            newsHead = Label(newsFrame, text="{}".format(newsList[k])).grid(row=k, column=0)
+            newsButton = Button(newsFrame, text='보기').grid(row=k, column=1)
+
 
     # 뉴스 출력/프레임
     newsLabel = Label(newWindow, text='<<{}월 {}일 오늘 실시간 인기뉴스>>'.format(mm.get(), dd.get()))
-    newsLabel.place(rely=0.4)
+    newsLabel.place(rely=0.37)
     newsFrame = Frame(newWindow, bg='#eaeaea')
-    newsFrame.place(relx=0.5, rely=0.45, relwidth=0.9, relheight=0.4, anchor='n')
+    newsFrame.place(relx=0.5, rely=0.43, relwidth=0.9, relheight=0.43, anchor='n')
     newsCrawler()
 
     buttonFrame = Frame(newWindow)
-    buttonFrame.place(relx=0.2, rely=0.9, relwidth=0.8, relheight=0.05)
-    button1 = Button(buttonFrame, text='순위표 보기',command = therank)
+    buttonFrame.place(relx=0.2, rely=0.91, relwidth=0.8, relheight=0.05)
+    button1 = Button(buttonFrame, text='순위표 보기',command = theRank)
     button1.place(relx=0, relwidth=0.3, relheight=1)
-    button2 = Button(buttonFrame, text='닫기')
+    button2 = Button(buttonFrame, text='닫기', command = root.quit)
     button2.place(relx=0.5, relwidth=0.3, relheight=1)
 
 #순위표창
-def therank():
+def theRank():
   rank = Toplevel()
   rankCanvas = Canvas(rank, height=400, width=400)
   rankCanvas.pack()
+  rankFrame = Frame(rank)
+  rankFrame.place(relx=0.5, rely=0.1, relwidth=0.9, relheight=0.9, anchor='n')
+
+  def showRank(): #순위표 출력
+    url = 'https://sports.news.naver.com/kbaseball/record/index.nhn?category=kbo&year=2020'
+    webpage = requests.get(url)
+    soup = BeautifulSoup(webpage.content, 'html.parser')
+
+    rankList = []
+    for i in range(11):
+        ranking = soup.select('#regularTeamRecordList_table > tr:nth-child({0}) > td.tm'.format(i))
+        for ra in ranking:
+            ra = str(ra)
+            ra = re.sub('<.+?>', '', ra, 0).strip()
+            rankList.append(ra)
+
+    rankNumList = []
+    for k in range(1, 11, 1):
+        rankNum = soup.select('#regularTeamRecordList_table > tr:nth-child({0}) > th'.format(k))
+        for rn in rankNum:
+            rn = str(rn)
+            rn = re.sub('<.+?>', '', rn, 0).strip()
+            rankNumList.append(rn)
+
+    rankLabel1 = Label(rankFrame, text="[현재 순위]").pack()
+    for k in range(len(rankList)): #순위출력
+        rankLabel2 = Label(rankFrame, text="{}위: {}".format(rankNumList[k],rankList[k])).pack()
+  showRank()
+
 
 # 메인창
 root = Tk()
